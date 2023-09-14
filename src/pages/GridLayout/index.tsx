616c697a6248fb39aa0ee329ec329f465bb2f752
HarrisonLin <lsfpositivee@163.com>
2023-09-14 10:27:24 +0800

    feat: add RGL demo

--- src/pages/GridLayout/index.tsx
+++ src/pages/GridLayout/index.tsx
@@ -1,14 +1,48 @@
-import GridLayout from 'react-grid-layout';
+import RGL, { Responsive, WidthProvider } from 'react-grid-layout';
+// import GridLayout, { Responsive, WidthProvider } from 'react-grid-layout';
+import Layout from '@/Layouts/BasicLayout';
+import { useMemo } from 'react';
+import '/node_modules/react-grid-layout/css/styles.css';
+import '/node_modules/react-resizable/css/styles.css';
+
+const ReactGridLayout = WidthProvider(RGL);
+// const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const MyGridLayout = () => {
+  // const ResponsiveReactGridLayout = useMemo(() => WidthProvider(Responsive), []);
+
+  const defaultGridProps = {
+    isDraggable: true,
+    isResizable: true,
+    items: 20,
+    rowHeight: 30,
+    width: 1200,
+    onLayoutChange: (layout: any) => {},
+    cols: 24,
+    style: { background: 'green' },
+  };
+
+  const onLayoutChange = (layout) => {
+    defaultGridProps.onLayoutChange(layout);
+  };
+
   // layout is an array of objects, see the demo for more complete usage
   const layout = [
-    { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
-    { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: 'c', x: 4, y: 0, w: 1, h: 2 },
+    { i: 'a', x: 0, y: 0, w: 6, h: 2, minW: 6, minH: 2 },
+    { i: 'b', x: 6, y: 0, w: 12, h: 2 },
+    { i: 'c', x: 18, y: 0, w: 6, h: 2 },
+    { i: 'd', x: 0, y: 2, w: 6, h: 2 },
+    { i: 'e', x: 6, y: 2, w: 12, h: 2 },
+    { i: 'f', x: 18, y: 2, w: 6, h: 2 },
   ];
   return (
-    <GridLayout style={{ background: 'green' }} layout={layout} cols={12} rowHeight={30} width={600}>
+    <ReactGridLayout
+      {...defaultGridProps}
+      layout={layout}
+      onLayoutChange={onLayoutChange}
+      //   breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+      //   cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+    >
       <div key="a" style={{ background: 'red' }}>
         a
       </div>
@@ -18,7 +52,16 @@ const MyGridLayout = () => {
       <div key="c" style={{ background: 'yellow' }}>
         c
       </div>
-    </GridLayout>
+      <div key="d" style={{ background: 'pink' }}>
+        d
+      </div>
+      <div key="e" style={{ background: 'purple' }}>
+        e
+      </div>
+      <div key="f" style={{ background: 'orange' }}>
+        f
+      </div>
+    </ReactGridLayout>
   );
 };
 
